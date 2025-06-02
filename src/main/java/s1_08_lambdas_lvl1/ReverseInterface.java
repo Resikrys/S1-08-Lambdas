358c03e5c3d951fd05008cbbb23a3a3c0c596c26
resikrys <resicris@gmail.com>
2025-06-02 18:32:30 +0200

    add reverse interface & main code variables

---
+++ src/main/java/s1_08_lambdas_lvl1/ReverseInterface.java
@@ -0,0 +1,6 @@
+package s1_08_lambdas_lvl1;
+
+@FunctionalInterface
+public interface ReverseInterface {
+    String reverse(String newString);
+}
--- src/main/java/s1_08_lambdas_lvl1/Task8.java
+++ src/main/java/s1_08_lambdas_lvl1/Task8.java
@@ -8,6 +8,9 @@ import static s1_08_lambdas_lvl1.SortStrings.randomNames;
 public class Task8 {
     public static void main(String[] args) {
         PiInterface piValue = () -> 3.1415;
+        ReverseInterface reversor = newString -> new StringBuilder(newString).reverse().toString();
+        String stringTobeReversed = "Banana";
+        String stringReversed = reversor.reverse(stringTobeReversed);
 
         namesWithO.forEach(System.out::println);
         namesWithOAndLength5.forEach(System.out::println);
@@ -19,5 +22,8 @@ public class Task8 {
 
         SortStrings.sortNamesFromShortestToLargest(randomNames);
         SortStrings.sortNamesFromLongestToShortest(randomNames);
+
+        System.out.println("Original string: " + stringTobeReversed);
+        System.out.println("Reversed string: " + stringReversed);
     }
 }
